29a83a49dda846d214d3961de1a19f192dea9d91
NiklasHoffmann <hoffmann.niklas@googlemail.com>
2025-11-11 11:34:21 +0100

    feat(utils): Add validation utilities
    - Add Ethereum address validation
    - Add token ID validation
    - Add URL and IPFS hash validation
    - Add email validation
    - Add NFT metadata validation
    - Add string sanitization for XSS prevention

---
+++ src/utils/validation/index.ts
@@ -0,0 +1,11 @@
+﻿/**
+ * VALIDATION UTILITIES - Input-Validierung & Sanitization
+ * 
+ * Validierungs-Utilities fÃ¼r Datensicherheit:
+ * â€¢ Addresses: Ethereum-Adress-Validierung (0x Format, Checksum)
+ * â€¢ NFT Data: Token-ID, Contract-Address, Chain-ID Validation
+ * â€¢ User Input: XSS-Prevention, SQL-Injection-Schutz
+ * â€¢ URL/Media: Sichere Media-URL Validierung
+ */
+
+export * from './general';
